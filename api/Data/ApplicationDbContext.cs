8a629428172a3fccf71bb39e13f528f6d4a11c90
Pop Mihai <105228436+mihai1910@users.noreply.github.com>
2024-05-06 21:11:15 +0300

    Simple CRUD jokes app

---
+++ api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<api.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ api/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string JokeQ { get; set; }
+        public string JokeA { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
